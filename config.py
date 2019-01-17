41dfbc2a49c91c8fc4c00fb0eb602cd2b3324631
Michał Leśniewski <mlesniew@gmail.com>
2019-01-17 22:29:36 +0100

    Add basic implementation

---
+++ config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import json
+
+B = [1 << i for i in range(7)]
+B1, B2, B3, B4, B5, B6, B7 = B
+
+data = {
+    'Parter': B1 | B2 | B3,
+    'Piętro': B4 | B5 | B6 | B7,
+    'Salon': B2 | B3,
+    'Sypialnia': B4,
+    'Hania': B5,
+    'Zuzia': B6,
+    'Biuro': B7,
+    'Kotłownia': B1,
+    'Salon L': B2,
+    'Salon R': B3,
+}
+
+if False:
+    # minified
+    print(json.dumps(data, separators=(',', ':')))
+else:
+    # pretty
+    print(json.dumps(data, indent=4, separators=(',', ': ')))
