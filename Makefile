2c2105f73e11ce73c16ecbb22640077cf7622812
Michał Leśniewski <mlesniew@gmail.com>
2019-01-19 21:27:17 +0100

    Improve serving of static content

--- Makefile
+++ Makefile
@@ -1,30 +1,25 @@
 ARDUINO=~/arduino-1.8.7/arduino
 FILE=rolek.ino
+STATIC_FILES=$(shell find static -type f)
 
 all: upload
 
-build: index.ino config.ino index.ino
+build: static.ino rolek.ino
 	$(ARDUINO) --verify $(FILE)
 
-upload: index.ino config.ino index.ino
+upload: static.ino rolek.ino
 	$(ARDUINO) --port /dev/ttyUSB0 --upload $(FILE)
 
 server:
-	python3 -m http.server
+	cd static; python3 -m http.server
 
 clean:
-	rm -f index.mini.html config.ino index.ino config.json
+	rm -f static.ino static/config.json
 
-index.mini.html: index.html
-	minify $< > $@
-
-index.ino: index.mini.html
-	xxd -i $< | sed -r 's#unsigned#const unsigned#' | sed 's#unsigned char#unsigned char PROGMEM#' | sed 's#mini_##' > $@
-
-config.json: config.py
+static/config.json: config.py
 	python3 $< > $@
 
-config.ino: config.json
-	xxd -i $< | sed -r 's#unsigned#const unsigned#' | sed 's#unsigned char#unsigned char PROGMEM#' > $@
+static.ino: $(STATIC_FILES)
+	./bin2ino.py $^ > $@
 
 .PHONY: all build upload server clean
---
+++ bin2ino.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import os
+import textwrap
+import sys
+import gzip
+import mimetypes
+
+symbols = {}
+
+for idx, filename in enumerate(sys.argv[1:], 1):
+    with open(filename, 'rb') as f:
+        data = f.read()
+    data = gzip.compress(data)
+    mime = mimetypes.guess_type(filename)[0]
+    size = len(data)
+    name = os.path.basename(filename)
+    symbol = name.upper().replace('.', '_')
+
+    code = ', '.join('0x%02x' % c for c in data)
+    code = 'const unsigned char PROGMEM %s[] = { %s };' % (symbol, code)
+
+    print('// %s' % filename)
+    for line in textwrap.wrap(code, 72):
+        print(line)
+    print()
+
+    symbols[name] = {
+            'mime': mime,
+            'symbol': symbol,
+            'size': size,
+            }
+
+print('void setup_static_endpoints(ESP8266WebServer & server) {')
+
+for name, info in symbols.items():
+    handler = info['symbol'].lower() + '_handler';
+    print('''    auto %s = [&]{
+        server.sendHeader("Content-Encoding", "gzip");
+        server.send_P(200,
+                "%s",
+                (PGM_P)(%s),
+                %i);
+        };''' % (handler, info['mime'], info['symbol'], info['size']))
+
+    names = [name]
+    if name in ('index.htm', 'index.html'):
+        names.append('')
+
+    for alias in names:
+        print('    server.on("/%s", HTTP_GET, %s);' % (alias, handler))
+
+    print()
+
+print('}')
