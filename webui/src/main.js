cceaf54c83978f96781651d19ff32c49078f97c1
Michał Leśniewski <mlesniew@gmail.com>
2022-04-11 21:49:36 +0200

    Use Pulse bootswatch theme

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import "../node_modules/bootswatch/dist/pulse/bootstrap.min.css";
 
 createApp(App).mount("#app");
